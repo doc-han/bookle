2222b180d8a18051082a3a1c52c3da1ba186d023
Han <yahyafarhan48@gmail.com>
2019-06-07 21:04:31 +0000

    init

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function createWindow () {
   mainWindow.loadFile('public/index.html')
   mainWindow.setMenu(null)
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //npm  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
